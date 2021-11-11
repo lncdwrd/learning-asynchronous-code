04523d91a3bc0d0b7d297ff6364b48d5e52fa5af
lncdwrd <lancebelarmino14@gmail.com>
2021-11-11 18:56:26 +0800

    initial commit

---
+++ promises.js
@@ -0,0 +1,60 @@
+/**
+ * 
+ * Under The Hood Promises
+ * 1. CustomPromise runs doWork()
+ * 2. JE (Javascript Engine) detects setTimeout so doWork() is set aside.
+ * 3. Javascript Engine continues parsing code while Browser finishes the setTimeout task.
+ * 4. 
+ */
+
+function CustomPromise(executor) {
+  let state = 'PENDING';
+  let value = null;
+  let handlers = [];
+  let catches = [];
+
+  function resolve(result) {
+    if (state !== 'PENDING') return;
+
+    state = 'FULFILLED';
+    value = result;
+
+    console.log('Resolve(): Executing Handlers');
+    handlers.forEach((h) => h(value));
+  }
+
+  function reject(err) {
+    if (state !== 'PENDING') return;
+
+    state = 'REJECTED';
+    value = err;
+
+    catches.forEach((c) => c(err));
+  }
+
+  this.then = function (callback) {
+    if (state === 'FULFILLED') {
+      console.log('.then(): Executing Callback');
+      callback(value);
+    } else {
+      console.log('.then(): Adding Callback To Handlers Array');
+      handlers.push(callback);
+    }
+  }
+
+  executor(resolve, reject);
+}
+
+const doWork = (res, rej) => {
+  console.log('Resolving in 1 second...');
+
+  setTimeout(() => { 
+    res('Hello World');
+  }, 1000);
+}
+
+let someText = new CustomPromise(doWork);
+
+someText.then((val) => {
+  console.log(`1st log: ${val}`)
+});
